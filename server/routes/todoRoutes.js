c332e905b813f0c4bfc2d602d54372e0e911a44e
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2024-01-09 02:31:33 +0500

    Swagger was Added

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const toDo = require('../models/ToDo')
 const { getTodo, createToDo, getAllTodos, updateTodo, deleteTodo } = require('../middlewares/todoMiddleware');
-const { error } = require('console');
 
 const router = express.Router();
 
 // GET ALL TASKS
-router.get('/', getAllTodos)
+router.get('/todos', getAllTodos)
 
-// CREATE A NEW TASK
-router.post('/', createToDo)
+// CREATE NEW TASK
+router.post('/todos', createToDo)
 
 // GET TASK BY ID
-router.get('/:id', getTodo, (req, res) => {
+router.get('/todos/:id', getTodo, (req, res) => {
     res.json(res.todo)
 })
 
 // UPDATE TASK BY ID
-router.patch('/:id', getTodo, updateTodo);
+router.patch('/todos/:id', getTodo, updateTodo);
 
 // DELETE TODO BY ID
-router.delete('/:id', getTodo, deleteTodo);
+router.delete('/todos/:id', getTodo, deleteTodo);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,30 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const toDoRoutes = require('./routes/todoRoutes')
-
 const cors = require('cors');
-const app = express()
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const toDoRoutes = require('./routes/todoRoutes');
+
+const app = express();
+
+// Загрузка файла Swagger
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 app.use(cors());
 app.use(express.json());
-app.use('/', toDoRoutes)
+
+// Настройка Swagger UI
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', toDoRoutes);
 
 const PORT = process.env.SERVER_PORT || 4000;
 const HOST = process.env.SERVER_HOST || '0.0.0.0';
 
 mongoose.connect(`${process.env.DB_MONGO_URL}`)
-    .then(() => console.log('Conndected to MongoDB'))
-    .catch((err) => console.error("Could connect to DB", err))
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error("Could not connect to DB", err));
 
 app.listen(PORT, () => {
     console.log(`TODO Server has been started on: http://${HOST}:${PORT}`);
-})
+});
