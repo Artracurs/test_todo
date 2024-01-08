aa4ae6f63dbb02e2063eda9f56df8436417a60e0
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2024-01-08 22:41:19 +0500

    create a new task route

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const toDo = require('../models/ToDo')
+
 const router = express.Router();
 
+// GET ALL TASKS
 router.get('/', async (req, res) => {
+    try {
+        const todos = await toDo.find();
+        res.json(todos)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
     res.send('response success')
+})
+
+// CREATE A NEW TASK
+router.post('/', async (req, res) => {
+    const todo = new toDo({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+    })
 })
 
 module.exports = router;
